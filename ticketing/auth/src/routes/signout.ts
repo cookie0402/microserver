88957f9ccc1f1bebd886068677583d94f6dbb9d8
cookie <chenqian0402@outlook.com>
2023-07-14 15:42:13 -0400

    sign out and current user

--- ticketing/auth/src/routes/signout.ts
+++ ticketing/auth/src/routes/signout.ts
@@ -1,7 +1,9 @@
 import express from "express";
 const router = express.Router();
 router.post('/api/users/signout', (req, res) => {
-  res.send('Hi,there sign out');
+  req.session = null;
+
+  res.send({});
 
 });
 export { router as signoutRouter }
